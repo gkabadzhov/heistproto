f8ea5f77e86e7c746f4424e0ea23c4c902c2a5d0
Georgi Kabadzhov <kabadzhov.georgi@gmail.com>
2024-07-12 14:21:01 +0300

    Adding level scene using the NavigationArea node

---
+++ Scenes/start_screen.gd
@@ -0,0 +1,11 @@
+class_name StartScreen extends Control
+
+func _on_button_button_up() -> void:
+	pass
+	#SceneManager.load_new_scene("","")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	#if Input.is_action_just_pressed("ui_accept"):
+	#	SceneManager.load_new_scene("","")
---
+++ Scripts/CharacterNavAgent.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+@export var target: Node2D = null
+
+var speed = 50
+var acceleration = 5
+
+@onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
+
+func _ready():
+	call_deferred("setup_navAgent")
+	
+	
+func setup_navAgent():
+	await get_tree().physics_frame
+	if target:
+		navigation_agent.target_position = target.global_position
+
+func _process(delta):
+	if navigation_agent.is_navigation_finished():
+		return
+		
+	var current_agent_position = global_position
+	var next_path_position = navigation_agent.get_next_path_position()
+	velocity = current_agent_position.direction_to(next_path_position) * speed
+	
+	print(velocity)
+	move_and_slide()
