ec5070885bf4c6346e52c4ce4f6e59415329d7af
Georgi Kabadzhov <kabadzhov.georgi@gmail.com>
2024-12-03 11:15:30 +0200

    Resolving an issue with emmitting civlian completed signal. New Bug - all actors are triggered at the same time on a second iteration

--- Scripts/Managers/CivilManager.gd
+++ Scripts/Managers/CivilManager.gd
@@ -5,24 +5,29 @@ class_name CivilManager
 @export var civil_interaction_points: Array[Node2D] = []
 @export var civil_starting_points: Array[Node2D] = []
 var civil_list: Array[AICharacter] = []
+var existing_active_civil = false
 
 func _ready():
 	for child in get_children():
 		if child is AICharacter:
 			civil_list.append(child)
+			child.connect("completed_action", Callable( self, "on_actor_completed_action"))
 
-	self.connect("completed_action", Callable( self, "on_actor_completed_action"))
-
-func handle_civil_routines(): 
+func handle_civil_routines():
+	if existing_active_civil:
+		return 
+		
 	var available_staff_point = get_parent().staff_manager.get_available_interaction_points()
 	
 	if available_staff_point != null:
 		var target_point = available_staff_point
-		var random_civil = civil_list[randi() % civil_list.size()]
+		var random_civil_index = randi() % civil_list.size()
 		
-		get_parent().staff_manager.disable_interaction_point(true)
-		
-		random_civil.move_to_interaction_point(target_point)
+		for civilian in civil_list:
+			if civilian == civil_list[random_civil_index]:
+				get_parent().staff_manager.disable_interaction_point(true)
+				civilian.move_to_interaction_point(target_point)
+				existing_active_civil = false
 
 func on_actor_completed_action():
 	handle_civil_routines()
